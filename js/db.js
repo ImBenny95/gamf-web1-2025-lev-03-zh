cdfba836226f3aaac9bc1195517831ad6bfb89c8
ImBenny95 <gm.silverss@gmail.com>
2025-05-10 10:18:08 +0200

    V3

--- js/db.js
+++ js/db.js
@@ -1,11 +1,9 @@
-const apiUrl = 'https://nettuts.hu/jms/ImBenny95/'; // csak a gyökér URL
+const apiUrl = 'https://nettuts.hu/jms/ImBenny95/';
 
-// Az összes adat lekérdezése GET metódussal
 export const getAll = (entity = 'cinema') => {
   return fetch(apiUrl + entity).then(res => res.json());
 };
 
-// Egy adott elem törlése ID alapján
 export const remove = (id, entity = 'cinema') => {
   return fetch(apiUrl + entity + '/' + id, {
     method: 'DELETE'
